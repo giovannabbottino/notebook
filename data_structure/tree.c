e943f626ed9758979217f77c023406965c3d6ab7
giovannabbottino <giovannabbottino@gmail.com>
2022-01-09 14:43:00 -0300

    Update tree

--- data_structure/tree.c
+++ data_structure/tree.c
@@ -1,132 +1,126 @@
-/*Giovanna Borges Bottino giovannabbottino@gmail.com*/
+/*
+ * Giovanna Borges Bottino 
+ * giovannabbottino@gmail.com
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 typedef struct node{
-    int data;
-    struct Node *left;
-    struct Node *right;
+    int key;
+    struct node *left;
+    struct node *right;
 } Node;
 
-Node *createNode(int data);
-Node *createTree(Node *tree, int data);
-Node *removeNode(Node *tree, int data);
-void printTreeInorder(Node *node);
-void printTreePreorder(Node *node);
-void printTreePostorder(Node *node);
+Node * createNode(int key);
+Node * createTree(Node * tree, int key);
+Node * removeNode(Node * tree, int key);
+Node * inorderSuccessor(Node * node);
+void printTreeInorder(Node * node);
+void printTreePreorder(Node * node);
+void printTreePostorder(Node * node);
 
 int main(){
     Node * tree = NULL;
-    int data;
-
-    while(scanf(" %d", &data) != EOF){
-        tree = createTree(tree, data);
+    int arr[7] = { 20, 10, 15, 30, 25, 35, 33 };
+    for (int i =0; i<7; i++){
+        tree = createTree(tree, arr[i]);
     }
+
     printTreePreorder(tree);
     printf(".\n");
+    
     printTreeInorder(tree);
     printf(".\n");
+    
     printTreePostorder(tree);
+    printf(".\n");
+
+    removeNode(tree, 30);
+    printTreeInorder(tree);
     printf(".\n");
 
     return 0;
 }
 
-Node *createNode(int data){
+Node *createNode(int key){
     Node* node = (Node *)malloc(sizeof(Node));
  
-    node->data = data;
+    node->key = key;
     
     node->left = NULL;
     node->right = NULL;
     return node;
 }
 
-Node *createTree(Node *node, int data){
+Node *createTree(Node * node, int key){
     if (node == NULL)
-        return createNode(data);
+        return createNode(key);
  
-    if (data < node->data)
-        node->left = createTree(node->left, data);
-    else if (data > node->data)
-        node->right = createTree(node->right, data);
+    if (key < node->key)
+        node->left = createTree(node->left, key);
+    else if (key > node->key)
+        node->right = createTree(node->right, key);
  
     return node;
 }
 
-Node *removeNode(Node *node, int data){
+Node *removeNode(Node * node, int key){
     if (node==NULL){
         return NULL;
-    } else if ( data < node->data){
-        node->left = removeNode(node, data);
-    } else if ( data > node->data){
-        node->right = removeNode(node, data);
+    } 
+    if ( key < node->key){
+        node->left = removeNode(node->left, key);
+    } else if ( key > node->key){
+        node->right = removeNode(node->right, key);
     } else {
-        if (node->left == NULL & node->right == NULL){
+        if(node->left == NULL){
+            Node *aux =  node->right;
             free(node);
-            node = NULL;
-        } else if(node->left == NULL){
-            Node *aux = node;
-            node = node->right;
-            free(aux);
-
+            return aux;
         } else if(node->right == NULL){
-            Node *aux = node;
-            node = node->left;
-            free(aux);
-        }
+            Node *aux =  node->left;
+            free(node);
+            return aux;
+        } else{
+            Node *inorder = inorderSuccessor(node->right);
+
+            node->key = inorder->key;
+
+            node->right = removeNode(node->right , inorder->key);
+        } 
+        return node;
     }
 }
 
-// Inorder 
-void printTreeInorder(Node *node){
-    if (node){
-        /*
-        *       15
-        *     /    \
-        *    10     50
-        *          /
-        *         20
-        * printf = 10 15 20 50
-        */     
+Node *inorderSuccessor(Node * node) {
+  while (node && node->left != NULL)
+    node = node->left;
+
+  return node;
+}
+
+void printTreeInorder(Node * node){
+    if (node){ 
         printTreeInorder(node->left);
-        printf("%d ", node->data);
+        printf("%d ", node->key);
         printTreeInorder(node->right);
     }
 }
-
-// Preorder  
-void printTreePreorder(Node *node){
+ 
+void printTreePreorder(Node * node){
     if (node){
-        /*
-        *       15
-        *     /    \
-        *    10     50
-        *          /
-        *         20
-        * printf = 15 10 50 20
-        */     
-        printf("%d ", node->data);
+        printf("%d ", node->key);
         printTreePreorder(node->left);
         printTreePreorder(node->right);
     }
 }
-
-// Postorder   
-void printTreePostorder(Node *node){
+ 
+void printTreePostorder(Node * node){
     if (node){
-        /*
-        *       15
-        *     /    \
-        *    10     50
-        *          /
-        *         20
-        * printf = 10 20 50 15
-        */     
         printTreePostorder(node->left);
         printTreePostorder(node->right);
-        printf("%d ", node->data);
+        printf("%d ", node->key);
     }
 }
